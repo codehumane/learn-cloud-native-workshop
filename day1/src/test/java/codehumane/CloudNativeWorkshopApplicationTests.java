127c07dc38e13b891a4c40ee63680e6a481c4ffc
codehumane <codehumane@gmail.com>
2017-01-02 21:53:51 +0900

    Cloud Native Workshop - Bootcamp
    [참고](https://github.com/joshlong/cloud-native-workshop#1-bootcamp)

--- day1/src/test/java/codehumane/CloudNativeWorkshopApplicationTests.java
+++ day1/src/test/java/codehumane/CloudNativeWorkshopApplicationTests.java
@@ -1,16 +1,41 @@
 package codehumane;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = CloudNativeWorkshopApplication.class)
+@WebAppConfiguration
 public class CloudNativeWorkshopApplicationTests {
 
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void before() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
+				.build();
+	}
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/reservations"))
+				.andExpect(MockMvcResultMatchers.content().contentType(
+				        MediaType.parseMediaType("application/hal+json;charset=UTF-8")))
+				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
 }
