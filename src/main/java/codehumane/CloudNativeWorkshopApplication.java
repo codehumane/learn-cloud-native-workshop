951478f8b221f4af943be06d65e30a39b96feec0
codehumane <codehumane@gmail.com>
2017-01-02 20:02:11 +0900

    Cloud Native Workshop - Setup
    - [참고](https://github.com/joshlong/cloud-native-workshop#setup)

---
+++ src/main/java/codehumane/CloudNativeWorkshopApplication.java
@@ -0,0 +1,12 @@
+package codehumane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNativeWorkshopApplication.class, args);
+	}
+}
