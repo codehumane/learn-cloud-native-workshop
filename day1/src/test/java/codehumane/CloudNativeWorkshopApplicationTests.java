b80605d001eff1c6d0638253528b2d7266c4197f
codehumane <codehumane@gmail.com>
2017-01-09 20:57:43 +0900

    spring boot 1.4에서부터는 SpringJUnit4ClassRunner가 SpringRunner라는 이름으로 간소화
    - [참고](https://spring.io/blog/2016/04/15/testing-improvements-in-spring-boot-1-4)

--- day1/src/test/java/codehumane/CloudNativeWorkshopApplicationTests.java
+++ day1/src/test/java/codehumane/CloudNativeWorkshopApplicationTests.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -14,7 +14,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = CloudNativeWorkshopApplication.class)
 @WebAppConfiguration
 public class CloudNativeWorkshopApplicationTests {
